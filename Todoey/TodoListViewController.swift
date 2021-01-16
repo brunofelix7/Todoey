7e9f0775d04444b476794280995080d19d80f356
Bruno Felix <brunofelix.dev@gmail.com>
2021-01-16 14:15:37 -0300

    Section 1 - Class 3. TableView Delegate Methods and Accessories

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -8,15 +8,29 @@ class TodoListViewController: UITableViewController {
         super.viewDidLoad()
     }
 
-    //  MARK: TableView Datasource Methods
+    //  MARK: TableView Datasource - Cria as rows de acordo com o tamanho do array
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
+    //  MARK: TableView Datasource - Itera sobre o array e preenche as rows com o valor de cada iteração
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
+    }
+    
+    //  MARK: TableView Delegate - Captura a row clicada
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //  Retorna a row clicada
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        //  Adiciona uma animação de clique nas rows
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
 }
